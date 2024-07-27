ec63554709433774455a041180cf54121763dfbb
julia jiang <157035850+juliajiang218@users.noreply.github.com>
2024-07-27 17:57:22 -0400

    Implement initial file and folder structure.

---
+++ app/(auth)/log-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      Login page.tsx
+    </div>
+  )
+}
+
+export default Login
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      (root) page.tsx
+    </div>
+  )
+}
+
+export default Home
